d09da1459cad14ebe58f7870f8f788da62513cf8
C.-W. HSU <11950284+chiaweh2@users.noreply.github.com>
2024-06-12 15:11:34 -0600

    include monthly report script and template
    needed resource is also included for a more complete package

---
+++ mhw_report_jinia_templating.py
@@ -0,0 +1,99 @@
+import calendar
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from bs4 import BeautifulSoup
+from jinja2 import Environment, FileSystemLoader
+
+def parse_draft():
+    """
+    The draft is the direct Google Drive document export html file
+
+    """
+    # Read the HTML file
+    draft_file = '/Public/chsu/share_mhw/DraftofGlobalMHWForecastDiscussion.html'
+    with open(draft_file, 'r', encoding='utf-8') as file:
+        html_content = file.read()
+
+    # Parse the HTML content
+    html_parsed = BeautifulSoup(html_content, 'html.parser')
+
+    current_paragraph1 = html_parsed.find_all('p')[1].text.replace("\xa0", " ")
+
+    current_paragraph2 = html_parsed.find_all('p')[2].text.replace("\xa0", " ")
+
+    forecast_paragraph1 = html_parsed.find_all('p')[3].text.replace("\xa0", " ")
+
+    list_regions = []
+
+    for li in html_parsed.find_all('li'):
+        try:
+            region = li.text.split("\xa0- ")[0]
+            region_text = li.text.split("\xa0- ")[1].replace("\xa0", " ")
+        except IndexError:
+            region, region_text = html_parsed.find_all('li')[3].text.split("-",1)
+
+
+        list_regions.append({
+            'region_name':region,
+            'region_text':region_text
+        })
+
+    return [
+        current_paragraph1,
+        current_paragraph2,
+        forecast_paragraph1,
+        list_regions
+    ]
+
+def render_html(year=2000,month=6,current=True):
+    """
+    render the html template with the information from the 
+    draft from google doc.
+
+    """
+    output_file = '/home/chsu/MHW/mhw_report_new.html'
+
+    month_strings = calendar.month_name[1:]
+
+    if current :
+        current_datetime = datetime.now()
+        year = current_datetime.year
+        month = current_datetime.month
+
+    # start date string
+    start_year = year
+    start_month = month_strings[month-1]
+    start_date = datetime(year=year, month=month, day=1)
+
+    # end date string
+    end_date = start_date + relativedelta(months=11)
+    end_year = end_date.year
+    end_month = month_strings[end_date.month-1]
+
+    # get draft content
+    replace_text = parse_draft()
+
+    # open template file
+    environment = Environment(
+        loader=FileSystemLoader("/home/chsu/MHW/mhw_report_template/")
+    )
+    template = environment.get_template("mhw_report_template.html")
+    rendered_html = template.render(
+        start_year = start_year,
+        start_month = start_month,
+        end_year = end_year,
+        end_month = end_month,
+        current_paragraph1 = replace_text[0],
+        current_paragraph2 = replace_text[1],
+        forecast_paragraph1 = replace_text[2],
+        list_regions = replace_text[3]
+    )
+
+    with open(output_file, mode="w", encoding="utf-8") as html_file:
+        html_file.write(rendered_html)
+        print(f"... {output_file} rendered")
+
+
+if __name__ == "__main__":
+
+   render_html(current=True)
---
+++ mhw_report_mailing.py
@@ -0,0 +1,120 @@
+import datetime
+import subprocess
+import sys
+from bs4 import BeautifulSoup
+
+
+def construct_body(soup):
+    """
+    Contructing the body of the email based on 
+    the set format of marine heatwave report page.
+
+    If the structure of the page change the
+    code in this function might need to be adjusted.
+    """
+    # Find a title
+    title = soup.find('h2')  # Example: Extract text within a <p> tag
+    title = f"<h2> {title.text} </h2>"
+
+    # Find banner
+    banner1 = soup.find('div', class_='alert-secondary')
+    banner1 = '<p> ('+banner1.text.strip() + ') <p>'
+
+    banner2 = soup.find('div', class_='alert-danger')
+    banner2 = '<p> ('+banner2.text.strip() + ') <p>'
+
+    # Find a forecast time
+    forecast_initime = f"<p> {soup.find_all('h5')[0].text} </p>"
+    forecast_period = f"<p> {soup.find_all('h5')[1].text} </p>"
+    add_link = '<p> For details and a more interactive interface, please visit https://psl.noaa.gov/marine-heatwaves/#report </p>'
+
+    # parsed subtitle and content in order
+    subtitle1 = f"<h3> [{soup.find_all('h3')[0].text}] </h3>"
+    content1 = f"<p> {soup.find('p', class_='smallP').text.strip()} </p>"
+
+    subtitle2 = f"<h4> {soup.find_all('h4')[0].text} </h4>"
+    # without warning new method banner
+    content21 = f"<p> {soup.find_all('p')[1].text.strip()} </p>"
+    content22 = f"<p> {soup.find_all('p')[2].text.strip()} </p>"
+    # with warning new method banner
+    # content21 = f"<p> {soup.find_all('p')[3].text.strip()} </p>"
+    # content22 = f"<p> {soup.find_all('p')[4].text.strip()} </p>"
+
+    subtitle3 = f"<h4> {soup.find_all('h4')[1].text} </h4>"
+    # without warning new method banner
+    content3 = f"{soup.find_all('p')[3]}"
+    # with warning new method banner
+    # content3 = f"{soup.find_all('p')[5]}"
+
+    all_content = (
+        "Content-Type: text/html\n\n<html><body>"+
+        title+
+        banner1+
+        banner2+
+        forecast_initime+
+        forecast_period+
+        add_link+
+        "<p>===========</p>"+
+        subtitle1+
+        content1+
+        "<p>---</p>"+
+        subtitle2+
+        content21+
+        content22+
+        "<p>---</p>"+
+        subtitle3+
+        content3+
+        "</body></html>"
+    )
+
+    return all_content
+
+def construct_html_body(soup):
+    """
+    Contructing the body of the email based on 
+    the set format of marine heatwave report page.
+
+    If the structure of the page change the
+    code in this function might need to be adjusted.
+    """
+    print('test')
+
+def send_email(recip_email, sub, body):
+    """
+    The function send the email out using the 
+    linux/unix sendmail function
+    """
+
+    sendemail = f'echo -e "To: {recip_email}\nSubject: {sub}\n{body}" | sendmail -t'
+
+    use_sendmail = subprocess.call(
+        sendemail,
+        shell=True,
+        executable="/usr/bin/bash"
+        )
+
+
+if __name__ == "__main__":
+
+    # get the date when the email be generated
+    date = datetime.date.today()
+    try:
+        if sys.argv[1] == 'mlist':
+            RECIPIENT_EMAIL = 'psl.mhw.forecast@list.woc.noaa.gov'
+        elif sys.argv[1] == 'test':
+            RECIPIENT_EMAIL = 'chia-wei.hsu@noaa.gov'
+    except IndexError:
+        sys.exit('please put argument value of "mlist" or "test"')
+
+    SUBJECT = f'NEW!!! {date.strftime("%Y %B")} Marine Heatwave Forecast Discussion'
+
+    # Read the HTML file
+    with open('/httpd-test/psd/marine-heatwaves/report.html', 'r') as file:
+        html_content = file.read()
+
+    # Parse the HTML content
+    html_parsed = BeautifulSoup(html_content, 'html.parser')
+    BODY = construct_body(html_parsed)
+
+    # send out the email
+    send_email(RECIPIENT_EMAIL, SUBJECT, BODY)
